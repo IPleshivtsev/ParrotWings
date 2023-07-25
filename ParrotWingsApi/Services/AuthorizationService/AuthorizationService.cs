f673d19708da091d478298997a2b4f426bb28693
Ilya.Pleshivtsev <ilya.pleshivtsev@yandex.ru>
2023-07-25 18:48:22 +0300

    Backend. Добавили сервис авторизации.

---
+++ ParrotWingsApi/Services/AuthorizationService/AuthorizationService.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ParrotWingsApi.Services.AuthorizationService;
+
+/// <summary>
+/// Сервис авторизации
+/// </summary>
+public class AuthorizationService : IAuthorizationService
+{
+    public IConfiguration _configuration { get; }
+
+    public AuthorizationService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    /// <summary>
+    /// Генерация токена на основе идентификатора пользователя
+    /// </summary>
+    /// <param name="userId">Идентификатор пользователя</param>
+    /// <returns>Сгенерированный токен</returns>
+    public string GenerateToken(Guid userId)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_configuration["Jwt:SecretKey"]);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim("UserId", userId.ToString()),
+            }),
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var jwtToken = tokenHandler.WriteToken(token);
+        return jwtToken;
+    }
+}
---
+++ ParrotWingsApi/Services/AuthorizationService/IAuthorizationService.cs
@@ -0,0 +1,14 @@
+﻿namespace ParrotWingsApi.Services.AuthorizationService;
+
+/// <summary>
+/// Интерфейс сервиса авторизации
+/// </summary>
+public interface IAuthorizationService
+{
+    /// <summary>
+    /// Генерирование токена на основе электронной почты пользователя
+    /// </summary>
+    /// <param name="userId">Идентификатор пользователя</param>
+    /// <returns>Сгенерированный токен</returns>
+    string GenerateToken(Guid userId);
+}
