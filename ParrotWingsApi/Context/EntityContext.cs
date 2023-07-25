f102445653f0130f14eb905d63b6f7d5e3116df5
Ilya.Pleshivtsev <ilya.pleshivtsev@yandex.ru>
2023-07-25 18:44:09 +0300

    Backend. Добавили контекст БД.

---
+++ ParrotWingsApi/Context/EntityContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParrotWingsApi.Data.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ParrotWingsApi.Context;
+
+/// <summary>
+/// Контекст доступа к БД.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class EntityContext : DbContext
+{
+    public EntityContext(DbContextOptions options) 
+        : base(options) { }
+
+    /// <summary>
+    /// Список пользователей
+    /// </summary>
+    public virtual DbSet<User>? Users { get; set; }
+
+    /// <summary>
+    /// Список транзакций
+    /// </summary>
+    public virtual DbSet<Transaction>? Transactions { get; set; }
+
+    /// <summary>
+    /// Список обращений
+    /// </summary>
+    public virtual DbSet<Appeal>? Appeals { get; set; }
+}
