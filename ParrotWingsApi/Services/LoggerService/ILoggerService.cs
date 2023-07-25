9a13c7081e6596b9b7fe4b8210692b24b0d53e0c
Ilya.Pleshivtsev <ilya.pleshivtsev@yandex.ru>
2023-07-25 18:48:43 +0300

    Backend. Добавили сервис логирования.

---
+++ ParrotWingsApi/Services/LoggerService/ILoggerService.cs
@@ -0,0 +1,24 @@
+﻿namespace ParrotWingsApi.Services.LoggerService;
+
+/// <summary>
+/// Интерфейс сервиса логирования
+/// </summary>
+public interface ILoggerService
+{
+    /// <summary>
+    /// Логирование сообщения ошибки
+    /// </summary>
+    /// <param name="message">Сообщение ошибки</param>
+    void Error(string message);
+
+    /// <summary>
+    /// Логирование информационного сообщения
+    /// </summary>
+    /// <param name="message">Информационное сообщение</param>
+    void Info(string message);
+
+    /// <summary>
+    /// Завершение логирования метода
+    /// </summary>
+    void EndLogMethod();
+}
---
+++ ParrotWingsApi/Services/LoggerService/LoggerService.cs
@@ -0,0 +1,69 @@
+﻿namespace ParrotWingsApi.Services.LoggerService;
+
+/// <summary>
+/// Модель сервиса логирования
+/// </summary>
+public class LoggerService : ILoggerService
+{
+    /// <summary>
+    /// Путь к выходному файлу
+    /// </summary>
+    public string _filePath;
+
+    /// <summary>
+    /// Инициализация логирования
+    /// </summary>
+    /// <param name="pathLog">Путь к папке с логами</param>
+    public LoggerService(string pathLog)
+    {
+        if (!Directory.Exists(pathLog))
+        {
+            Directory.CreateDirectory(pathLog);
+        }
+
+        _filePath = pathLog + $"\\LogApi_{DateTime.Now.ToShortDateString()}.txt";
+    }
+
+    /// <summary>
+    /// Логирование сообщения ошибки
+    /// </summary>
+    /// <param name="message">Сообщение ошибки</param>
+    public void Error(string message)
+    {
+        LogMessage(
+            string.Format("{0} Error: {1}\n",
+            DateTime.Now.ToLongTimeString(), 
+            message));
+    }
+
+    /// <summary>
+    /// Логирование информационного сообщения
+    /// </summary>
+    /// <param name="message">Информационное сообщение</param>
+    public void Info(string message)
+    {
+        LogMessage(string.Format(
+            "{0} Info: {1}\n", 
+            DateTime.Now.ToLongTimeString(),
+            message));
+    }
+
+    /// <summary>
+    /// Добавление сообщения в файл логирования
+    /// </summary>
+    /// <param name="message">Сообщение</param>
+    private void LogMessage(string message)
+    {
+        File.AppendAllText(
+            _filePath, 
+            message);
+    }
+
+    /// <summary>
+    /// Завершение логирования метода
+    /// </summary>
+    public void EndLogMethod()
+    {
+        LogMessage("\n");
+    }
+}
